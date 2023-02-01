196cf325f16a1a57e280e7ee9859583f4c96fdb5
raashidbaba <raashidbaba849@gmail.com>
2023-02-01 21:41:09 +0530

    Fixed the intent Filter and added intent to Main Activity.

--- app/src/main/java/com/devedroy/mydoc/MainActivity.kt
+++ app/src/main/java/com/devedroy/mydoc/MainActivity.kt
@@ -1,11 +1,18 @@
 package com.devedroy.mydoc
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val goToSignbtn = findViewById<Button>(R.id.button)
+        goToSignbtn.setOnClickListener {
+            val intent = Intent(this, SignUpActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
