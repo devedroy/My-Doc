dd9e5463b4a33451105facfbd9477ab986f42e24
Deved Roy <devedroy.dr@gmail.com>
2023-02-02 09:37:08 +0530

    Code Refactoring done.

--- app/src/main/java/com/devedroy/mydoc/SignUpActivity.kt
+++ app/src/main/java/com/devedroy/mydoc/SignUpActivity.kt
@@ -3,7 +3,7 @@ package com.devedroy.mydoc
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
-class SignUpActivity :AppCompatActivity(){
+class SignUpActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.sign_up)
