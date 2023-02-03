041e1c29ac313bb0f7c5d4cfc1886c582b11987a
raashidbaba <raashidbaba849@gmail.com>
2023-02-03 13:12:06 +0530

    Added LoginActivity.kt and activity_login.xml done some refactoring.

--- app/src/main/java/com/devedroy/mydoc/SignUpActivity.kt
+++ app/src/main/java/com/devedroy/mydoc/SignUpActivity.kt
@@ -1,11 +1,19 @@
 package com.devedroy.mydoc
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
 class SignUpActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.sign_up)
+        val sendMeToLoginBtn = findViewById<Button>(R.id.btsignup)
+
+        sendMeToLoginBtn.setOnClickListener {
+            val intent = Intent(this,LoginActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
