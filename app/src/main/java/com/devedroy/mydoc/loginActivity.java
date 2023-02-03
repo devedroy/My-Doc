c546cef06315938ff13b8eb41cf840bd72718413
balrammane99 <balrammane3307@gmail.com>
2023-02-03 19:51:37 +0530

    written code

---
+++ app/src/main/java/com/devedroy/mydoc/loginActivity.java
@@ -0,0 +1,69 @@
+package com.devedroy.mydoc;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.Task;
+
+public class loginActivity extends AppCompatActivity {
+
+    Button signingoogel;
+
+    GoogleSignInOptions gso;
+    GoogleSignInClient gsc;
+    @SuppressLint("WrongViewCast")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        signingoogel=findViewById(R.id.tvsignupwith);
+
+        gso=new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).requestEmail().build();
+        gsc=GoogleSignIn.getClient(this,gso);
+        signingoogel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SignIn();
+            }
+        });
+
+    }
+
+    private void SignIn() {
+        Intent intent=gsc.getSignInIntent();
+        startActivityForResult(intent,100);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode==100){
+            Task<GoogleSignInAccount>task=GoogleSignIn.getSignedInAccountFromIntent(data);
+            try {
+                task.getResult(ApiException.class);
+                HomeActivity();
+            } catch (ApiException e) {
+                Toast.makeText(this,"error",Toast.LENGTH_LONG).show();
+            }
+        }
+
+    }
+
+    private void HomeActivity() {
+        finish();
+        Intent intent=new Intent(getApplicationContext(),HomeActivity.class);
+        startActivity(intent);
+    }
+}
