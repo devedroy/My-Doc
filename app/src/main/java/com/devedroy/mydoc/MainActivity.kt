fdbd44cb4532d527ba63aa37cc09fc67b2a41863
devedroy <devedroy.dr@gmail.com>
2023-02-04 19:40:01 +0530

    Code indented.

--- app/src/main/java/com/devedroy/mydoc/MainActivity.kt
+++ app/src/main/java/com/devedroy/mydoc/MainActivity.kt
@@ -4,8 +4,6 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
-//comment by dev
-
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
