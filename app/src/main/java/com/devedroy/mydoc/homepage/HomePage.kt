fdd3523bbc3d181b8df39effebfe409334b74202
devedroy <devedroy.dr@gmail.com>
2023-02-04 20:03:56 +0530

    viewBinding initialized.

--- app/src/main/java/com/devedroy/mydoc/homepage/HomePage.kt
+++ app/src/main/java/com/devedroy/mydoc/homepage/HomePage.kt
@@ -3,10 +3,20 @@ package com.devedroy.mydoc.homepage
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.devedroy.mydoc.R
+import com.devedroy.mydoc.databinding.ActivityHomePageBinding
 
 class HomePage : AppCompatActivity() {
+
+    lateinit var binding: ActivityHomePageBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home_page)
+        binding = ActivityHomePageBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        configureHospitalAdapter()
+    }
+
+    private fun configureHospitalAdapter() {
+        TODO("Not yet implemented")
     }
 }
